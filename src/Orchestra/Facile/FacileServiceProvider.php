3722689a2dd9e19c0c528b07c006fa07f82f0fa4
crynobone <crynobone@gmail.com>
2013-04-02 23:19:10 +0800

    Add ServiceProvider and Facade
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Facile/FacileServiceProvider.php
@@ -0,0 +1,42 @@
+<?php namespace Orchestra\Facile;
+
+use Illuminate\Support\ServiceProvider;
+
+class FacileServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerEnvironment();
+		$this->registerTemplate();
+	}
+
+	/**
+	 * Register Facile environment.
+	 *
+	 * @access protected
+	 * @return void
+	 */
+	protected function registerEnvironment()
+	{
+		$this->app['orchestra.facile'] = $this->app->share(function($app)
+		{
+			return new Environment;
+		});
+	}
+
+	/**
+	 * Register Facile environment.
+	 *
+	 * @access protected
+	 * @return void
+	 */
+	protected function registerEnvironment()
+	{
+		$this->app['orchestra.facile']->template('default', new Template);
+	}
+}
