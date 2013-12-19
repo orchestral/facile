9cf1b9a919ed039241dee05c57d102b02a669307
crynobone <crynobone@gmail.com>
2013-12-19 17:41:15 +0800

    Fixed service provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Facile/FacileServiceProvider.php
+++ src/Orchestra/Facile/FacileServiceProvider.php
@@ -19,10 +19,10 @@ class FacileServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bindShared('orchestra.facile', function ($app) {
-            $env = new Environment;
+            $env = new Environment($app['request']);
 
             $env->template('default', function () use ($app) {
-                return new Template\Base($app['request'], $app['view']);
+                return new Template\Base($app['view']);
             });
 
             return $env;
