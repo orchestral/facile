cf10193d63d33a7df3b718bdcae446d055eae4cc
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-04 15:41:23 +0800

    Add ability to export excel.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Template/Composers/Excel.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Orchestra\Facile\Template\Composers;
+
+use Maatwebsite\Excel\Excel as Exporter;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Contracts\Support\Arrayable;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+trait Excel
+{
+    /**
+     * Compose XLS.
+     *
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $config
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function composeXls(array $data = [], int $status = 200, array $config = []): SymfonyResponse
+    {
+        return $this->composeExcel($data, $config, Exporter::XLS);
+    }
+
+    /**
+     * Compose XLSX.
+     *
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $config
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function composeXlsx(array $data = [], int $status = 200, array $config = []): SymfonyResponse
+    {
+        return $this->composeExcel($data, $config, Exporter::XLSX);
+    }
+
+    /**
+     * Compose excel.
+     *
+     * @param  array  $data
+     * @param  array  $config
+     * @param  string  $writerType
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function composeExcel(array $data, array $config, string $writerType): SymfonyResponse
+    {
+        $filename = $config['filename'] ?? 'export';
+
+        return $this->convertToExcel($data, $config)->exportExcel($filename, $writerType);
+    }
+
+    /**
+     * Convert content to CSV.
+     *
+     * @param  array  $data
+     * @param  array  $config
+     *
+     * @return \Orchestra\Support\Collection
+     */
+    protected function convertToExcel(array $data, array $config): Collection
+    {
+        $uses = $config['uses'] ?? 'data';
+        $content = $data[$uses] ?? [];
+
+        if ($content instanceof Arrayable) {
+            $content = $content->toArray();
+        }
+
+        return Collection::make(\array_map([$this, 'transformToArray'], $content));
+    }
+}
